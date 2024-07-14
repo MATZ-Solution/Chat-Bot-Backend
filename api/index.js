4abff4511ad2b2711c0989c4489b44159b58123b
Mansoor Ahmed Memon <mansoorahmed.one@gmail.com>
2024-07-14 20:40:48 +0500

    Add endpoint for fetching records from a specific category using filters

--- api/index.js
+++ api/index.js
@@ -1,164 +1,133 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const { MongoClient } = require("mongodb");
-var cors = require("cors");
-
-const app = express();
-app.use(cors());
-app.use(bodyParser.json());
-
-const PORT = 5000;
 const URI = "mongodb+srv://matzsolutions:2VVG2QxBAMub9Oaz@cluster0.gyal2.mongodb.net";
+const DB_NAME = "healthcare";
 
-const client = new MongoClient(URI);
-client.connect();
-const dbName = "healthcare";
-
-const database = client.db(dbName);
-console.log("Connected to MongoDB");
-
-const getSortedData = async (collection, query, top) => {
-    if (top) {
-        query.scrapedAverageRating = { $exists: true };
-    }
-    const sortCriteria = top ? { scrapedAverageRating: -1, name: 1 } : { name: 1 };
-    const data = await collection.find(query).sort(sortCriteria).limit(5).toArray();
-    return data;
+const CATEGORY_MAPPINGS = {
+    "nursing-homes": "nursingHomenew",
+    "memory-care": "memoryCare",
+    "in-home-care": "inHomeCare",
+    "inpatient-rehabilitations": "inpatientrehabilitiations",
 };
 
-app.get("/api/:collectionKeyword", async (req, res) => {
-    try {
-        const { collectionKeyword } = req.params;
-        const { top } = req.query;
-        const collection = database.collection(collectionKeyword);
-        const sortCriteria = top ? { scrapedAverageRating: -1, name: 1 } : { name: 1 };
-        const data = await collection.find().sort(sortCriteria).limit(5).toArray();
+const express = require("express");
+const app = express();
 
-        res.json(data);
-    } catch (err) {
-        console.error(`Something went wrong trying to find the documents: ${err}\n`);
-        res.status(500).json({ error: "Internal Server Error" });
-    }
+const cors = require("cors");
+app.use(cors());
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+const { MongoClient } = require("mongodb");
+const client = new MongoClient(URI);
+client.connect();
+
+const database = client.db(DB_NAME);
+console.log("Connected to MongoDB!");
+
+// const getSortedData = async (collection, query, top) => {
+//     if (top) {
+//         query.scrapedAverageRating = { $exists: true };
+//     }
+//     const sortCriteria = top ? { scrapedAverageRating: -1, name: 1 } : { name: 1 };
+//     const data = await collection.find(query).sort(sortCriteria).limit(5).toArray();
+//     return data;
+// };
+
+// app.get("/api/:collectionKeyword", async (req, res) => {
+//     try {
+//         const { collectionKeyword } = req.params;
+//         const { top } = req.query;
+//         const collection = database.collection(collectionKeyword);
+//         const sortCriteria = top ? { scrapedAverageRating: -1, name: 1 } : { name: 1 };
+//         const data = await collection.find().sort(sortCriteria).limit(5).toArray();
+
+//         res.json(data);
+//     } catch (err) {
+//         console.error(`Something went wrong trying to find the documents: ${err}\n`);
+//         res.status(500).json({ error: "Internal Server Error" });
+//     }
+// });
+
+// // Route to fetch by zip code
+// app.get("/api/:collectionKeyword/zipCode/:zipCode", async (req, res) => {
+//     try {
+//         const { zipCode, collectionKeyword } = req.params;
+//         const { top } = req.query;
+//         const collection = database.collection(collectionKeyword);
+//         const data = await getSortedData(collection, { zipCode }, top);
+//         res.json(data);
+//     } catch (err) {
+//         console.error(`Error fetching ${collectionKeyword} by zip code: ${err}`);
+//         res.status(500).json({ error: "Internal Server Error" });
+//     }
+// });
+// app.get("/api/:collectionKeyword/city/:city", async (req, res) => {
+//     const { collectionKeyword, city } = req.params;
+//     try {
+//         const { top } = req.query;
+//         const collection = database.collection(collectionKeyword);
+//         const data = await getSortedData(collection, { city }, top);
+//         res.json(data);
+//     } catch (err) {
+//         console.error(`Something went wrong trying to find the documents: ${err}\n`);
+//         res.status(500).json({ error: "Internal Server Error" });
+//     }
+// });
+// app.get("/api/:collectionKeyword/state/:state", async (req, res) => {
+//     const { collectionKeyword, state } = req.params;
+//     try {
+//         const { top } = req.query;
+//         const collection = database.collection(collectionKeyword);
+//         const data = await getSortedData(collection, { state }, top);
+//         res.json(data);
+//     } catch (err) {
+//         console.error(`Something went wrong trying to find the documents: ${err}\n`);
+//         res.status(500).json({ error: "Internal Server Error" });
+//     }
+// });
+
+app.get("/", (req, res) => {
+    res.send("Welcome to the Nursing Home API 2.0");
 });
 
-// Route to fetch by zip code
-app.get("/api/:collectionKeyword/zipCode/:zipCode", async (req, res) => {
+app.get("/api/:category/fetch", async (req, res) => {
     try {
-        const { zipCode, collectionKeyword } = req.params;
-        const { top } = req.query;
-        const collection = database.collection(collectionKeyword);
-        const data = await getSortedData(collection, { zipCode }, top);
-        res.json(data);
-    } catch (err) {
-        console.error(`Error fetching ${collectionKeyword} by zip code: ${err}`);
-        res.status(500).json({ error: "Internal Server Error" });
-    }
-});
-app.get("/api/:collectionKeyword/city/:city", async (req, res) => {
-    const { collectionKeyword, city } = req.params;
-    try {
-        const { top } = req.query;
-        const collection = database.collection(collectionKeyword);
-        const data = await getSortedData(collection, { city }, top);
-        res.json(data);
-    } catch (err) {
-        console.error(`Something went wrong trying to find the documents: ${err}\n`);
-        res.status(500).json({ error: "Internal Server Error" });
-    }
-});
-app.get("/api/:collectionKeyword/state/:state", async (req, res) => {
-    const { collectionKeyword, state } = req.params;
-    try {
-        const { top } = req.query;
-        const collection = database.collection(collectionKeyword);
-        const data = await getSortedData(collection, { state }, top);
-        res.json(data);
-    } catch (err) {
-        console.error(`Something went wrong trying to find the documents: ${err}\n`);
-        res.status(500).json({ error: "Internal Server Error" });
-    }
-});
-app.get("/api/:collectionKeyword/search", async (req, res) => {
-    try {
-        const { collectionKeyword } = req.params;
-        const { city, state, zipCode } = req.query;
+        var { category } = req.params;
+        category = CATEGORY_MAPPINGS[category];
 
-        let findQuery = {};
+        const { zipCode, state, city, name, top } = req.query;
 
-        if (city) {
-            findQuery.city = city;
-        }
+        let filters = {};
 
-        if (state) {
-            findQuery.state = state;
-        }
+        console.log("FETCHING ...");
+        console.log(zipCode, state, city, name, category);
 
         if (zipCode) {
-            findQuery.zipCode = zipCode;
+            filters.zipCode = zipCode;
         }
 
-        const collection = database.collection(collectionKeyword);
-        const data = await collection.find(findQuery).sort({ name: 1 }).limit(7).toArray();
-
-        res.json(data);
-    } catch (error) {
-        console.error("Error in search API:", error);
-        res.status(500).json({ error: "Internal Server Error" });
-    }
-});
-app.get("/api/:collectionKeyword/city_state", async (req, res) => {
-    try {
-        const { collectionKeyword } = req.params;
-        const { city, state } = req.query;
-        let findQuery = {};
-        if (city) {
-            findQuery.city = city;
-        }
         if (state) {
-            findQuery.state = state;
+            filters.state = state;
         }
-        const collection = database.collection(collectionKeyword);
-        const data = await collection.find(findQuery).sort({ name: 1 }).limit(7).toArray();
-        res.json(data);
-    } catch (error) {
-        console.error("Error in search API:", error);
-        res.status(500).json({ error: "Internal Server Error" });
-    }
-});
-app.get("/api/:collectionKeyword/fullAddress", async (req, res) => {
-    try {
-        const { collectionKeyword } = req.params;
-        const { fullAddress } = req.query;
-        let findQuery = {};
-        if (fullAddress) {
-            findQuery.fullAddress = fullAddress;
+
+        if (city) {
+            filters.city = city;
         }
-        const collection = database.collection(collectionKeyword);
-        const data = await collection.find(findQuery).sort({ name: 1 }).limit(5).toArray();
+
+        if (name) {
+            filters.name = name;
+        }
+
+        const collection = database.collection(category);
+        const data = await collection.find(filters).sort({ name: 1 }).limit(7).toArray();
+
         res.json(data);
-    } catch (error) {
-        console.error("Error in search API:", error);
-        res.status(500).json({ error: "Internal Server Error" });
+    } catch (e) {
+        res.status(500).json({ error: e });
     }
 });
 
-// Example route for searching by exact name
-app.get("/api/:collectionKeyword/searchByName/:name", async (req, res) => {
-    try {
-        const { collectionKeyword, name } = req.params;
-        const findQuery = { name: { $regex: new RegExp(`^${name}$`, "i") } };
-        const collection = database.collection(collectionKeyword);
-        const data = await collection.find(findQuery).toArray();
-        res.json(data);
-    } catch (error) {
-        console.error("Error in searchByName API:", error);
-        res.status(500).json({ error: "Internal Server Error" });
-    }
-});
-app.get("/", (req, res) => {
-    res.send("Welcome to the Nursing Home API");
-});
-
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
